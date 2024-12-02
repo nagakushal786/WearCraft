6f5e23d087c9f8d195ba7b2a2a9dd5345c0f6ad4
Kushal <nagakushal10@gmail.com>
2024-12-03 00:19:59 +0530

    Backend index updated

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,10 @@ app.use(express.json({limit: "50mb"}));
 
 app.use("/api/v1/dalle", router);
 
-app.get("/", (req, res)=> {
-    return res.status(200).send("Welcome to DALL-E");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.listen(8080, ()=> {
